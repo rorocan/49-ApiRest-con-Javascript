92eb1f9e874a1b8f11bec0ca5b8f18c63fd5913c
Rodrigo Rojas <rorocanrojas1998@gmail.com>
2025-01-15 09:38:30 -0500

    mainprivate

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-console.log('API URL = https://api.themoviedb.org/3/movie/550?api_key=51bc316b3a8f2e7f0a1356496de79d9c' + API_KEY)
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
+    const data = await res.json();
+  
+    const movies = data.results;
+    movies.forEach(movie => {
+      const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+      
+      const movieContainer = document.createElement('div');
+      movieContainer.classList.add('movie-container');
+  
+      const movieImg = document.createElement('img');
+      movieImg.classList.add('movie-img');
+      movieImg.setAttribute('alt', movie.title);
+      movieImg.setAttribute(
+        'src',
+        'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+      );
+  
+      movieContainer.appendChild(movieImg);
+      trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+  }
+  
+  getTrendingMoviesPreview();
+//console.log('API URL = https://api.themoviedb.org/3/movie/550?api_key=51bc316b3a8f2e7f0a1356496de79d9c' + API_KEY)
